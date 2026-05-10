3209057d45818545a59e5f69b84cd3cad8f3f9e7
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 10:04:42 +0400

    add set and get methods

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -3,7 +3,7 @@ package Entity;
 import java.time.LocalDate;
 
 public class Person {
-    private String is;
+    private String id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
@@ -11,4 +11,53 @@ public class Person {
     private String phoneNumber;
     private String email;
     private String address;
+
+    public void setId(String id){
+        this.id=id;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getFirstName(){
+        return  firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth){
+        this.dateOfBirth=dateOfBirth;
+    }
+    public LocalDate getDateOfBirth(){
+        return dateOfBirth;
+    }
+    public void setGender(String gender){
+        this.gender=gender;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public String getAddress(){
+        return address;
+    }
 }
