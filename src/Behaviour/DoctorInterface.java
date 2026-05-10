9bca503b5a235294e3be668fedc37c0c994c2108
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 13:19:03 +0400

    add required methods in  DoctorInterface

--- src/Behaviour/DoctorInterface.java
+++ src/Behaviour/DoctorInterface.java
@@ -1,4 +1,11 @@
 package Behaviour;
 
+import Entity.Patient;
+
+import java.util.UUID;
+
 public interface DoctorInterface {
+    Boolean assignPatient(Patient patient);
+    Boolean removePatient(Patient patient);
+    Boolean updateAvailability(UUID uuid,Patient updatedPatient);
 }
