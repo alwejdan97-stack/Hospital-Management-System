e70c7b5b520278523303b912c89efd77d622778c
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 13:36:31 +0400

    add required methods in AppointmentInterface interface

--- src/Behaviour/AppointmentInterface.java
+++ src/Behaviour/AppointmentInterface.java
@@ -1,4 +1,9 @@
 package Behaviour;
 
+import Entity.Appointment;
+
 public interface AppointmentInterface {
+    Boolean reschedule(Appointment appointment);
+    Boolean cancel(Appointment appointment);
+    Boolean complete(Appointment appointmentComplete);
 }
