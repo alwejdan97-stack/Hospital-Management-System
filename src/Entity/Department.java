566f447203c657e54359323f2411faf497f5c4fd
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 13:43:04 +0400

    create set and get methods in Appointment class

--- src/Entity/Department.java
+++ src/Entity/Department.java
@@ -10,4 +10,17 @@ public class Department {
     private List<Nurse> nurses;
     private int bedCapacity;
     private int availableBeds;
+
+    public void setDepartmentId(String departmentId){
+        this.departmentId=departmentId;
+    }
+    public String getDepartmentId(){
+        return departmentId;
+    }
+    public void setDepartmentName(String departmentName){
+        this.departmentName=departmentName;
+    }
+    public String getDepartmentName(){
+        return departmentName;
+    }
 }
