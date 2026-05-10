5386b8ab1c39e55e43e8b8b70ea57ef36796e23f
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 12:45:33 +0400

    add set and get methods

--- src/Entity/Nurse.java
+++ src/Entity/Nurse.java
@@ -8,4 +8,32 @@ public class Nurse extends Person{
     private String shift;
     private String qualification;
     private List<Patient> assignedPatients;
+
+    public void setNurseId(String nurseId){
+        this.nurseId=nurseId;
+    }
+    public String getNurseId(){
+        return nurseId;
+    }
+    public void setDepartmentId(String departmentId){
+        this.departmentId=departmentId;
+    }
+    public String getDepartmentId(){
+        return departmentId;
+    }
+    public void setShift(String shift){
+        this.shift=shift;
+    }
+    public String getShift(){
+        return shift;
+    }
+    public void setQualification(String qualification){
+        this.qualification=qualification;
+    }
+    public void setAssignedPatients(List<Patient> assignedPatients){
+        this.assignedPatients=assignedPatients;
+    }
+    public List<Patient> getAssignedPatients(){
+        return assignedPatients;
+    }
 }
