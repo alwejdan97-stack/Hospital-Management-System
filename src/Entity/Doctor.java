a58b8ee59229b637c107ed75eafb458f682e70a4
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 12:16:18 +0400

    add required feild in Doctor class

--- src/Entity/Doctor.java
+++ src/Entity/Doctor.java
@@ -1,4 +1,14 @@
 package Entity;
 
+import java.util.List;
+
 public class Doctor extends Person{
+    private String doctorId;
+    private String specialization;
+    private String qualification;
+    private int experienceYears;
+    private String departmentId;
+    private double consultationFee;
+    private List<String> availableSlots;
+    private List<Patient> assignedPatients;
 }
