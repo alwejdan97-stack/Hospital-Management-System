86f44be8b263c17d874cf2d4733fd42e05aa465f
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 09:57:21 +0400

    add required feilds in Person class

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -1,4 +1,14 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class Person {
+    private String is;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
 }
