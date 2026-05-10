0f0fa8801e9425c444129a11590939d417b4f6dd
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 13:56:02 +0400

    add required method in DepartmentInterface interface

--- src/Behaviour/DepartmentInterface.java
+++ src/Behaviour/DepartmentInterface.java
@@ -1,4 +1,7 @@
 package Behaviour;
 
 public interface DepartmentInterface {
+    void assignDoctor(String doctorName);
+    void assignNurse(String nursName);
+    Boolean updateBedAvailability(int updatedBed);
 }
