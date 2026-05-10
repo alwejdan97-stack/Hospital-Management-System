361ddaf2cd18a6f09ae4661b12f49f5080b8f472
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 13:24:45 +0400

    add required feilds in Appointment class

--- src/Entity/Appointment.java
+++ src/Entity/Appointment.java
@@ -1,4 +1,14 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private String notes;
 }
