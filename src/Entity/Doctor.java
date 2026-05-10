458d7146afec0f6e6d2dd2a9dccda452a69b8e9c
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 12:30:35 +0400

    add set and get methods

--- src/Entity/Doctor.java
+++ src/Entity/Doctor.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Doctor extends Person{
     private String doctorId;
@@ -11,4 +12,57 @@ public class Doctor extends Person{
     private double consultationFee;
     private List<String> availableSlots;
     private List<Patient> assignedPatients;
+
+    public Doctor(){
+        super();
+    }
+
+    public void setDoctorId(String doctorId){
+        this.doctorId=doctorId;
+    }
+    public String getDoctorId(){
+        return doctorId;
+    }
+    public void setSpecialization(String specialization){
+        this.specialization=specialization;
+    }
+    public String getSpecialization(){
+        return specialization;
+    }
+    public void setQualification(String qualification){
+        this.qualification=qualification;
+    }
+    public String getQualification(){
+        return qualification;
+    }
+    public void setExperienceYears(int experienceYears){
+        this.experienceYears=experienceYears;
+    }
+    public int getExperienceYears(){
+        return experienceYears;
+    }
+    public void setDepartmentId(String departmentId){
+        this.departmentId=departmentId;
+    }
+    public String getDepartmentId(){
+        return departmentId;
+    }
+    public void setConsultationFee(double consultationFee){
+        this.consultationFee=consultationFee;
+    }
+    public double getConsultationFee(){
+        return consultationFee;
+    }
+    public void setAvailableSlots(List<String> availableSlots){
+        this.availableSlots=availableSlots;
+    }
+    public List<String> getAvailableSlots(){
+        return availableSlots;
+    }
+    public void setAssignedPatients(List<Patient> assignedPatients){
+        this.assignedPatients=assignedPatients;
+    }
+    public List<Patient> getAssignedPatients(){
+        return assignedPatients;
+    }
 }
