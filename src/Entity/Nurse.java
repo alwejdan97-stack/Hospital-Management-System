c0b136c14e9951b61f4b0b2604b8dd5574681129
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 12:40:14 +0400

    add required feild in Nurse class

--- src/Entity/Nurse.java
+++ src/Entity/Nurse.java
@@ -1,4 +1,11 @@
 package Entity;
 
+import java.util.List;
+
 public class Nurse extends Person{
+    private String nurseId;
+    private String departmentId;
+    private String shift;
+    private String qualification;
+    private List<Patient> assignedPatients;
 }
