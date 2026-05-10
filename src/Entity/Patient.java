a12d9d016a81254cd1b4ef37110eb167d49ef636
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-10 11:30:12 +0400

    add required feilds in Patient class

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -1,4 +1,15 @@
 package Entity;
 
-public class Patient {
+import java.time.LocalDate;
+import java.util.List;
+
+public class Patient extends Person{
+    private String patientId;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private String insuranceId;
+    private List<String> medicalRecords;
+    private List<String> appointments;
 }
