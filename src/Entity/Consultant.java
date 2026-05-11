ccb2d61ebb83623054e40266c173c48e0453de30
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 15:52:42 +0400

    create Consultant class

--- src/Entity/Consultant.java
+++ src/Entity/Consultant.java
@@ -1,4 +1,16 @@
 package Entity;
 
+import java.util.List;
+
 public class Consultant extends Doctor{
+    private List<String> consultationTypes;
+    private Boolean onlineConsultationAvailable;
+    private int consultationDuration;
+
+    public Consultant(List<String> consultationTypes, Boolean onlineConsultationAvailable, int consultationDuration) {
+        super();
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
 }
