7dadeac9b3f6a3bcf2768ce83bfa3eb4dce79e9a
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 11:45:50 +0400

    add required methods to handel MedicalRecordService class

--- src/Services/MedicalRecordService.java
+++ src/Services/MedicalRecordService.java
@@ -1,7 +1,12 @@
 package Services;
 
+import Entity.Doctor;
 import Entity.MedicalRecord;
+import Entity.Nurse;
+import Entity.Patient;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +15,15 @@ public class MedicalRecordService {
     public List<MedicalRecord> medicalRecordList=new ArrayList<>();
     public Scanner scanner=new Scanner(System.in);
     MedicalRecord medicalRecord=new MedicalRecord();
+    public Patient patient=new Patient();
+    public Doctor doctor=new Doctor();
+    public Nurse nurse=new Nurse();
+
+    PatientService patientService=new PatientService();
+    DoctorService doctorService=new DoctorService();
+    NurseService nurseService=new NurseService();
+
+    DateTimeFormatter formatter= DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public MedicalRecord addRecord(){
         System.out.println(" ** Adding new Record ** ");
@@ -18,6 +32,7 @@ public class MedicalRecordService {
         medicalRecord.setRecordId(scanner.nextLine());
         System.out.println("Enter Doctor ID: ");
         medicalRecord.setDoctorId(scanner.nextLine());
+
         System.out.println("Enter Patient ID: ");
         medicalRecord.setPatientId(scanner.nextLine());
         System.out.println("Enter Diagnosis: ");
@@ -25,7 +40,8 @@ public class MedicalRecordService {
         System.out.println("Enter Prescription: ");
         medicalRecord.setPrescription(scanner.nextLine());
         System.out.println("Enter Visit Date: ");
-        medicalRecord.setVisitDate();
+        LocalDate visitDate=LocalDate.parse(scanner.nextLine(),formatter);
+        medicalRecord.setVisitDate(visitDate);
         System.out.println("Enter Test Result:");
         medicalRecord.setTestResults(scanner.nextLine());
         System.out.println("Enter Notes: ");
@@ -36,10 +52,43 @@ public class MedicalRecordService {
     }
 
     public List<MedicalRecord> addRecords(){
-
+        Boolean continueFlag = true;
+        while (continueFlag) {
+            medicalRecordList.add(addRecord());
+            System.out.println("If want ot add more records press C");
+            if(scanner.nextLine().equalsIgnoreCase("C")){
+                continueFlag=true;
+            }
+            continueFlag=false;
+        }
+        return medicalRecordList;
     }
 
-    public void updateRecord(){}
+    public void updateRecord(){
+        displayAllRecords();
+        System.out.println("Enter Record ID");
+        String ID=scanner.nextLine();
+        Boolean found=false;
+        for(MedicalRecord m:medicalRecordList){
+            if(m.getRecordId().equalsIgnoreCase(ID)){
+                System.out.println("Enter New Record ID");
+                m.setRecordId(scanner.nextLine());
+                System.out.println("Record Updated Successfully");
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.println("Record NOT Found");
+        }
+        Boolean continueFlag = true;
+        while (continueFlag){
+            System.out.println("If want ot update more Records press C");
+            if(scanner.nextLine().equalsIgnoreCase("C")){
+                continueFlag=false;
+            }
+        }
+    }
 
     public void deleteRecord(){}
 
@@ -49,7 +98,14 @@ public class MedicalRecordService {
 
     public void displayPatientHistory(){}
 
-    public void displayAllRecords(){}
+    public void displayAllRecords(){
+        if(medicalRecordList.isEmpty()){
+            System.out.println("NO Record in the List");
+        }
+        for(MedicalRecord m: medicalRecordList) {
+            System.out.println("Record ID: "+m.getRecordId()+" | Patient ID: "+m.getPatientId()+" | Doctor ID: "+m.getDoctorId()+" | Diagnosis: "+m.getDiagnosis()+" | Prescription: "+m.getPrescription()+" | Visit Date: "+m.getVisitDate());
+        }
+    }
 
     public Boolean handelMedicalRecordService(Integer medicalRecordOption){
         switch (medicalRecordOption){
