bbc82aedfb8ad55cab2576ed52b785d1a8516e18
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 16:11:37 +0400

    create constructor inside EmergencyPatient class

--- src/Entity/EmergencyPatient.java
+++ src/Entity/EmergencyPatient.java
@@ -5,4 +5,12 @@ public class EmergencyPatient extends Patient {
     private String arrivalMode;
     private int triageLevel;
     private  Boolean admittedViaER;
+
+    public EmergencyPatient(String emergencyType, String arrivalMode, int triageLevel, Boolean admittedViaER) {
+        super();
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
 }
