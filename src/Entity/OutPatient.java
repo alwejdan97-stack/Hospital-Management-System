8460e36c1cd9bd4e2adb0a4ac14ed78fe0f69f0c
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 16:06:28 +0400

    create get and set methods inside class

--- src/Entity/OutPatient.java
+++ src/Entity/OutPatient.java
@@ -6,4 +6,11 @@ public class OutPatient extends Patient{
     private int visitCount;
     private LocalDate lastVisitDate;
     private String preferredDoctorId;
+
+    public OutPatient(int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        super();
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
+    }
 }
