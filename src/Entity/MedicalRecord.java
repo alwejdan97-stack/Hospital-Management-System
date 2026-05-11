a8ff2de88b939827b616bb8e950e84d5c83d0bd3
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 11:11:19 +0400

    updated code in class

--- src/Entity/MedicalRecord.java
+++ src/Entity/MedicalRecord.java
@@ -12,7 +12,8 @@ public class MedicalRecord {
     private String testResults;
     private String notes;
 
-    public MedicalRecord(String recordId, String patientId, String doctorId, LocalDate visitDate, String diagnosis, String prescription, String testResults, String notes) {
+    public MedicalRecord() {
+        super();
         this.recordId = recordId;
         this.patientId = patientId;
         this.doctorId = doctorId;
