da6c6037f929ee2768a3893615760a090eda40f8
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 16:10:48 +0400

    create EmergencyPatient class

--- src/Entity/EmergencyPatient.java
+++ src/Entity/EmergencyPatient.java
@@ -1,4 +1,8 @@
 package Entity;
 
-public class EmergencyPatient {
+public class EmergencyPatient extends Patient {
+    private String emergencyType;
+    private String arrivalMode;
+    private int triageLevel;
+    private  Boolean admittedViaER;
 }
