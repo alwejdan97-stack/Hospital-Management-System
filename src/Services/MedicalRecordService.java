267b5eeed4d368fb8d8a5ebeecaf71754e526600
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 11:04:00 +0400

    add required methods to handel MedicalRecordService class

--- src/Services/MedicalRecordService.java
+++ src/Services/MedicalRecordService.java
@@ -1,7 +1,52 @@
 package Services;
 
 public class MedicalRecordService {
+
+
     public void getRecordsByPatientId(){}
     public void getRecordsByDoctorId(){}
     public void displayPatientHistory(){}
+
+    public Boolean handelMedicalRecordService(Integer medicalRecordOption){
+        switch (medicalRecordOption){
+            case 1 -> {
+                System.out.println("== Add New Patient ==");
+                addPatients();
+            }
+            case 2 -> {
+                System.out.println("== Update Patient ==");
+                editPatient();
+            }
+
+            case 3 -> {
+                System.out.println("== Show Patients ==");
+                displayAllPatients();
+            }
+
+            case 4 -> {
+                System.out.println("== Show SPECIFIC Patient Using ID ==");
+                getPatientById();
+            }
+
+            case 5 -> {
+                System.out.println("== Delete Patient ==");
+                removePatient();
+            }
+
+            case 6 -> {
+                System.out.println("== Search For a Patient ==");
+                searchPatientsByName();
+            }
+
+            case 7 ->
+            {
+                return false;
+            }
+
+            default ->{
+                System.out.println("Invalid Option");
+            }
+        }
+        return true;
+    }
 }
