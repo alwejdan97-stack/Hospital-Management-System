d13741bbb2bacb803523c7b870e9700ab00f48e0
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 15:57:02 +0400

    create GeneralPractitioner class

--- src/Entity/GeneralPractitioner.java
+++ src/Entity/GeneralPractitioner.java
@@ -1,4 +1,8 @@
 package Entity;
 
 public class GeneralPractitioner extends Doctor {
+    private Boolean walkinAvailable;
+    private Boolean homeVisitAvailable;
+    private Boolean vaccinationCertified;
+
 }
