0f030befe7515a5b7ab28bfa39bdecfb2decbb63
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 11:02:27 +0400

    add methods in MedicalRecordService class

--- src/Services/MedicalRecordService.java
+++ src/Services/MedicalRecordService.java
@@ -1,4 +1,7 @@
 package Services;
 
 public class MedicalRecordService {
+    public void getRecordsByPatientId(){}
+    public void getRecordsByDoctorId(){}
+    public void displayPatientHistory(){}
 }
