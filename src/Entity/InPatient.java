ad76761778b9b313e16db1dd0245161f6556da55
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 16:02:20 +0400

    create get and set methods inside class

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -9,4 +9,14 @@ public class InPatient extends Patient {
     private String bedNumber;
     private String admittingDoctorId;
     private Double dailyCharges;
+
+    public InPatient(LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, Double dailyCharges) {
+        super();
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber = roomNumber;
+        this.bedNumber = bedNumber;
+        this.admittingDoctorId = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
+    }
 }
