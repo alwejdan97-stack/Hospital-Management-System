e7738dabca6266c0b8b33f8213215365aaed78c8
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 16:05:57 +0400

    create OutPatient class

--- src/Entity/OutPatient.java
+++ src/Entity/OutPatient.java
@@ -1,4 +1,9 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class OutPatient extends Patient{
+    private int visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
 }
