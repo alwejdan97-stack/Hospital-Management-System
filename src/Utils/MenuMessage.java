5bca7ad8ea81daa489053ec329aab89dd4854e95
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-11 09:31:20 +0400

    add required message in main menu

--- src/Utils/MenuMessage.java
+++ src/Utils/MenuMessage.java
@@ -1,4 +1,55 @@
 package Utils;
 
 public class MenuMessage {
+    public static String MAIN_MENU_MESSAGE = """
+            1- Patient Menu
+            2- Doctor Menu
+            3- Nurse Menu
+            4- Department Menu
+            5- Medical Menu
+            6- Appointment Menu
+            7- Exit
+            """;
+    public static String PATIENT_MENU_MESSAGE = """
+            1- Add New Patient
+            2- Update Patient
+            3- Show Patients
+            4- Show SPECIFIC Patient Using ID
+            5- Delete Patient
+            6- Search For a Patient
+            7- Go back to Main Menu
+            """;
+
+    public static String DOCTOR_MENU_MESSAGE = """
+            1- ADD
+            2- UPDATE
+            3- DISPLAY
+            4- DISPLAY SPECIFIC DEPARTMENT
+            5- DELETE
+            6- Go Back to Main Menu
+            """;
+    public static String STUDENT_MENU_MESSAGE = """
+            1- ADD
+            2- UPDATE
+            3- DISPLAY
+            4- DISPLAY SPECIFIC STUDENT
+            5- DELETE
+            6- Go Back to Main Menu
+            """;
+    public static String TEACHER_MENU_MESSAGE = """
+            1- ADD
+            2- UPDATE
+            3- DISPLAY
+            4- DISPLAY SPECIFIC TEACHER
+            5- DELETE
+            6- Go Back to Main Menu
+            """;
+    public static String COURSE_MENU_MESSAGE = """
+            1- ADD
+            2- UPDATE
+            3- DISPLAY
+            4- DISPLAY SPECIFIC COURSE
+            5- DELETE
+            6- Go Back to Main Menu
+            """;
 }
