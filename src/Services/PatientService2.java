4f76228b925c5588974831523ab4348c9a0bb13e
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-12 08:28:41 +0400

    create overloading methods inside PatientService2 class

--- src/Services/PatientService2.java
+++ src/Services/PatientService2.java
@@ -10,7 +10,7 @@ public class PatientService2 {
     public List<Patient> patientList=new ArrayList<>();
 
     public Patient addPatient(String firstName, String lastName, String phone){
-        System.out.println("== with minimal information ==");
+        System.out.println("== Add patient with minimal information ==");
         patient.setFirstName(firstName);
         patient.setLastName(lastName);
         patient.setPhoneNumber(phone);
@@ -19,7 +19,7 @@ public class PatientService2 {
         return patient;
     }
     public Patient addPatient(String firstName, String lastName, String phone, String bloodGroup, String email){
-        System.out.println("== with with blood group and email ==");
+        System.out.println("== Add patient with with blood group and email ==");
         patient.setFirstName(firstName);
         patient.setLastName(lastName);
         patient.setPhoneNumber(phone);
@@ -31,20 +31,30 @@ public class PatientService2 {
     }
 
     public void addPatient(Patient patient){
-        System.out.println("== With Full Object ==");
+        System.out.println("== Add patient With Full Object ==");
         patientList.add(patient);
 
         System.out.println("Patient Added Successfully");
     }
 
     public void searchPatients(String keyword){
-        displayPatients();
+        System.out.println("== Search for patient by any field ==");
         for(Patient p: patientList){
-
+            if(p.getLastName().equalsIgnoreCase(keyword) ||p.getFirstName().equalsIgnoreCase(keyword) ||p.getPatientId().equalsIgnoreCase(keyword) ||p.getPhoneNumber().equalsIgnoreCase(keyword) ||p.getBloodGroup().equalsIgnoreCase(keyword) ||p.getInsuranceId().equalsIgnoreCase(keyword) ||p.getDateOfBirth().equalsIgnoreCase(keyword) ||p.getGender().equalsIgnoreCase(keyword) ||p.getEmail().equalsIgnoreCase(keyword) ||p.getMedicalRecords().equals(keyword)) {
+                System.out.println("Patient exist");
+            }
         }
+        System.out.println("Patient NOT exist");
     }
 
-    public void searchPatients(String firstName, String lastName){}
+    public void searchPatients(String firstName, String lastName){
+        for(Patient p:patientList){
+            if(p.getFirstName().equalsIgnoreCase(firstName)&&p.getLastName().equalsIgnoreCase(lastName)){
+                System.out.println("Patient exist");
+            }
+        }
+        System.out.println("Patient NOT exist");
+    }
 
     public void displayPatients(){
         for(Patient p:patientList){
