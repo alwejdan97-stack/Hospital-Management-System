5ed6f7d165c5cb56a34429963f0d84e6d22c1db5
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-13 08:25:22 +0400

    create methods inside HelperUtils class

--- src/Utils/HelperUtils.java
+++ src/Utils/HelperUtils.java
@@ -5,4 +5,14 @@ public abstract class HelperUtils {
     boolean isNull(String str);
     boolean isNotNull(Object obj);
     boolean isNotNull(String str);
+
+    public static boolean isValidString(String str);
+    public static boolean isValidString(String str, int minLength);
+    public static boolean isValidString(String str, int minLength, int maxLength);
+    public static boolean isValidString(String str, String regex);
+
+    void generateId();
+    void generateId(String prefix);
+    void generateId(String prefix, int length);
+    void generateId(String prefix, String suffix);
 }
