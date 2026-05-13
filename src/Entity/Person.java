5b3fa3d8b11aeddb4663b9429624e41012ba2fb8
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-13 11:12:07 +0400

    last modification in Person Class

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import Behaviour.Displayable;
+import Utils.HelperUtils;
 
 import java.util.Objects;
 
@@ -76,7 +77,7 @@ public class Person implements Displayable {
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+        if (HelperUtils.isNotNull(o) || getClass() != o.getClass()) return false;
         Person person = (Person) o;
         return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(dateOfBirth, person.dateOfBirth) && Objects.equals(gender, person.gender) && Objects.equals(phoneNumber, person.phoneNumber) && Objects.equals(email, person.email) && Objects.equals(address, person.address);
     }
