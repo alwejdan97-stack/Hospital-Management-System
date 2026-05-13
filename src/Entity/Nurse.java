3d027d58f822cf3c1b09c019af9437f64c4e9d01
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-13 10:26:32 +0400

    last modification in Nurse Class

--- src/Entity/Nurse.java
+++ src/Entity/Nurse.java
@@ -54,7 +54,7 @@ public class Nurse extends Person implements Displayable {
     @Override
     public void displayInfo(){
         super.displayInfo();
-        System.out.println("Nurs ID: "+nurseId);
+        System.out.println("Nurse ID: "+nurseId);
         System.out.println("Department ID: "+departmentId);
         System.out.println("Shift: "+shift);
         System.out.println("Qualification: "+qualification);
