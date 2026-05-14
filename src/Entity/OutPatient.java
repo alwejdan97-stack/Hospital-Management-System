c51e93c436f525f88c585ac6581ae9c10789684e
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-14 12:17:11 +0400

    new modification in Class

--- src/Entity/OutPatient.java
+++ src/Entity/OutPatient.java
@@ -9,6 +9,13 @@ public class OutPatient extends Patient implements Displayable {
     private LocalDate lastVisitDate;
     private String preferredDoctorId;
 
+    public OutPatient(int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        super();
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
+    }
+
     public OutPatient() {
         super();
         this.visitCount = visitCount;
