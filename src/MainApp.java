3ebf5b48ca0bebdd7e5c9dbdf4087b3f8636ef28
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-14 11:44:17 +0400

    new modification in MainApp Class

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,14 +1,15 @@
 //package ObjectOrientedProgramming.OOPDemo;
+import Entity.*;
 import Menu.Menu;
 import Services.*;
 import Utils.InputHandler;
 import Utils.MenuMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainApp {
-
-
     static Menu menu = new Menu();
     static Scanner input = new Scanner(System.in);
 
@@ -94,5 +95,64 @@ public class MainApp {
                 default -> System.out.println("Select a choice from the list");
             }
         }
+    }
+
+    public void dataAndTesting(){
+        List<Patient> patientList=new ArrayList<>();
+        List<Doctor> doctorList=new ArrayList<>();
+        List<Nurse> nurseList=new ArrayList<>();
+        List<Department> departmentList=new ArrayList<>();
+        List<MedicalRecord> medicalRecordList=new ArrayList<>();
+        List<Appointment> appointmentList=new ArrayList<>();
+
+        //patient testing
+        patientList.add(new Patient("P-01","Wejdan Salim","Female","Room-01"));
+        patientList.add(new InPatient("P-02","Hoor Omar","Female","Room-02"));
+        patientList.add(new OutPatient("P-03","Noor Ali","Female","Room-03"));
+        patientList.add(new EmergencyPatient("P-04","Ali Nasser","Male","Room-04"));
+
+        //doctor
+        doctorList.add(new Doctor("D-01","Wejdan Salim","Consultant","Cardiology"));
+        doctorList.add(new Doctor("D-02","Hoor Omar","Surgeon","Neurology"));
+        doctorList.add(new Consultant("D-03","Ali Ahmed","Consultant","General"));
+        doctorList.add(new Consultant("D-04","Sara Said","Consultant","Emergency"));
+        doctorList.add(new GeneralPractitioner("D-05","Fatema Ali","General Practitioner","Cardiology"));
+        doctorList.add(new GeneralPractitioner("D-06","Fatema Saif","General Practitioner","General"));
+        doctorList.add(new Surgeon("D-07","Tom Tomas","Surgeon","Emergency"));
+        doctorList.add(new Surgeon("D-08","Nasser Ali","Surgeon","Neurlogy"));
+
+        //nurse
+        nurseList.add(new Nurse("N-01","Wejdan Salim"));
+        nurseList.add(new Nurse("N-02","Hoor Said"));
+        nurseList.add(new Nurse("N-03","Omar Salim"));
+        nurseList.add(new Nurse("N-04","Sara Ali"));
+        nurseList.add(new Nurse("N-05","Sara Said"));
+
+        //department
+        departmentList.add(new Department("DEPT-1","Neurology"));
+        departmentList.add(new Department("DEPT-2","Cardiology"));
+        departmentList.add(new Department("DEPT-3","Emergency"));
+
+        //medical record
+        medicalRecordList.add(new MedicalRecord("MR-1","P001","Diabetes"));
+        medicalRecordList.add(new MedicalRecord("MR-2","P002","Diabetes"));
+        medicalRecordList.add(new MedicalRecord("MR-3","P003","Diabetes"));
+
+        //appointment
+        appointmentList.add(new Appointment("A01","P01","D01"));
+        appointmentList.add(new Appointment("A02","P02","D02"));
+        appointmentList.add(new Appointment("A03","P03","D03"));
+        appointmentList.add(new Appointment("A04","P04","D04"));
+        appointmentList.add(new Appointment("A05","P05","D05"));
+        appointmentList.add(new Appointment("A06","P06","D06"));
+        appointmentList.add(new Appointment("A07","P07","D07"));
+        appointmentList.add(new Appointment("A08","P08","D08"));
+        appointmentList.add(new Appointment("A09","P09","D09"));
+        appointmentList.add(new Appointment("A010","P010","D010"));
+        appointmentList.add(new Appointment("A011","P011","D011"));
+        appointmentList.add(new Appointment("A012","P012","D012"));
+        appointmentList.add(new Appointment("A013","P013","D013"));
+        appointmentList.add(new Appointment("A014","P014","D014"));
+        appointmentList.add(new Appointment("A015","P015","D015"));
     }
 }
